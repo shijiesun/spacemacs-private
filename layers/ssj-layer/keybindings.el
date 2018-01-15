2ffb12900ee1372eb8c2186ee3bed8d85c28b39a
ssj <ssj280@gmail.com>
2018-01-15 14:40:01 +0800

    a text-csale-increase

--- layers/ssj-layer/keybindings.el
+++ layers/ssj-layer/keybindings.el
@@ -1,3 +1,7 @@
 (global-set-key (kbd "C-c q") 'bing-dict-detail)
 
 (global-set-key (kbd "C-c w") 'copy-line)
+
+(global-set-key (kbd"C-+") 'text-scale-increase)
+
+(global-set-key (kbd"C--") 'text-scale-decrease)
